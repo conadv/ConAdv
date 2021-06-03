1a504e85dd003bbe84342b3589690088d2314242
conadv <85235781+conadv@users.noreply.github.com>
2021-06-03 10:11:10 +0800

    Update run.sh

--- models/official/resnet/run.sh
+++ models/official/resnet/run.sh
@@ -4,6 +4,6 @@ for warm_lr in  43.0
 do
  	for warm_epoch in 41
  	do
- 		python resnet_main.py     --tpu=v3-256   --data_dir=gs://imagenet2012_eu/imagenet-2012-tfrecord     --model_dir=$MODEL_DIR     --config_file=configs/cloud/v3-256.yaml    --mode=train --warm_lr=$warm_lr --warm_epoch=$warm_epoch
+ 		python resnet_main.py     --tpu=v3-1024   --data_dir=gs://imagenet2012_eu/imagenet-2012-tfrecord     --model_dir=$MODEL_DIR     --config_file=configs/cloud/v3-1024.yam    --mode=train --warm_lr=$warm_lr --warm_epoch=$warm_epoch
  	done
 done
